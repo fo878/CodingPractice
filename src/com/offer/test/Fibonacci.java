71da074be60aa84eed9d51dad58394e03b27715d
李耀皓 <278241740@qq.com>
2020-03-02 17:28:07 +0800

    新增

--- src/com/offer/test/Fibonacci.java
+++ src/com/offer/test/Fibonacci.java
@@ -39,6 +39,19 @@ public class Fibonacci {
         }
     }
 
+    //考虑大数越界
+    public int fib(int n) {
+        int a = 0, b = 1, sum;
+        for(int i = 0; i < n; i++){
+            sum = (a + b) % 1000000007;//十亿零七
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+
+
     public static void main(String[] args) {
         Fibonacci f = new Fibonacci();
         System.out.println(f.Fibonacci1(9));
---
+++ src/com/offer/test/findRepeatNumberSolution.java
@@ -0,0 +1,4 @@
+package com.offer.test;
+
+public class findRepeatNumberSolution {
+}
