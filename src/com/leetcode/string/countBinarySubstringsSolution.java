32f3631467f01e7c04ed2dcd4fd350874177ea62
李耀皓 <278241740@qq.com>
2020-08-13 18:56:20 +0800

    新增

---
+++ src/com/leetcode/string/countBinarySubstringsSolution.java
@@ -0,0 +1,28 @@
+package com.leetcode.string;
+/*
+* 696. 计数二进制子串
+*
+给定一个字符串 s，计算具有相同数量0和1的非空(连续)子字符串的数量，并且这些子字符串中的所有0和所有1都是组合在一起的。
+重复出现的子串要计算它们出现的次数。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/count-binary-substrings
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
+public class countBinarySubstringsSolution {
+
+    public int countBinarySubstrings(String s) {
+        int ptr = 0, n = s.length(), last = 0, ans = 0;
+        while (ptr < n) {
+            char c = s.charAt(ptr);
+            int count = 0;
+            while (ptr < n && s.charAt(ptr) == c) {
+                ++ptr;
+                ++count;
+            }
+            ans += Math.min(count, last);
+            last = count;
+        }
+        return ans;
+    }
+}
