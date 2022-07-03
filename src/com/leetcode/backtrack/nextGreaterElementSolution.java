c84dd3cba7d4e080359a2b12dddb8c964e3f331a
liyaohao <liyaohao@bjsasc.com>
2022-07-03 21:25:37 +0800

    下一个更大元素 III

---
+++ src/com/leetcode/backtrack/nextGreaterElementSolution.java
@@ -0,0 +1,53 @@
+package com.leetcode.backtrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * 给你一个正整数n ，请你找出符合条件的最小整数，其由重新排列 n中存在的每位数字组成，并且其值大于 n 。如果不存在这样的正整数，则返回 -1 。
+ *
+ * 注意 ，返回的整数应当是一个 32 位整数 ，如果存在满足题意的答案，但不是 32 位整数 ，同样返回 -1 。
+ *
+ */
+class nextGreaterElementSolution {
+    public int nextGreaterElement(int n) {
+        char[] nums = Integer.toString(n).toCharArray();
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i < 0) {
+            return -1;
+        }
+
+        int j = nums.length - 1;
+        while (j >= 0 && nums[i] >= nums[j]) {
+            j--;
+        }
+        swap(nums, i, j);
+        reverse(nums, i + 1);
+        long ans = Long.parseLong(new String(nums));
+        return ans > Integer.MAX_VALUE ? -1 : (int) ans;
+    }
+
+    public void reverse(char[] nums, int begin) {
+        int i = begin, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public void swap(char[] nums, int i, int j) {
+        char temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        nextGreaterElementSolution foo = new nextGreaterElementSolution();
+        foo.nextGreaterElement(123321);
+    }
+}
